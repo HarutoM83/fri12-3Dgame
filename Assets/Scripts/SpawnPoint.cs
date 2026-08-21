f511d798081072faa4ff88145609c5f8ddf2a3f1
HarutoM83 <ktc24a32a0010@edu.kyoto-tech.ac.jp>
2026-08-21 11:34:36 +0900

    スポーンポイントの修正

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -8,19 +8,7 @@ public class SpawnPoint : MonoBehaviour
     [SerializeField] GameObject enemy;
     [SerializeField] Transform spawnpoint;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Spawn();
     }
